c0d6b25a09e5113e2d465b71b57b7a74b3d23314
Metastem <110768529+Metastem@users.noreply.github.com>
2023-01-13 11:51:27 +0800

    Delete misc directory

--- misc/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="footer-container">
-    <a href="./">Home</a>
-    <a href="./api.php" target="_blank">API</a>
-    <a href="https://commune.metastem.su/about/" target="_blank">About</a>
-</div>
-</body>
-</html>
